8f1021be325e9149c6581044080b910dbbd520cd
Gürkan Vural <gurkan.vural@gmail.com>
2017-03-24 14:58:13 +0300

    recaptcha form

--- decaptcha/engines/recaptcha.py
+++ decaptcha/engines/recaptcha.py
@@ -13,33 +13,40 @@ logger = logging.getLogger(__name__)
 class RecaptchaEngine(object):
 
     CAPTCHA_XPATH = '//iframe[contains(@src, "google.com/recaptcha/api")]/@src'
+    CAPTCHA_FORM_XPATH = '//form[/script/contains(@src, "google.com/recaptcha/api")]'
 
     def __init__(self, crawler):
         self.crawler = crawler
 
     def has_captcha(self, response, **kwargs):
         sel = scrapy.Selector(response)
-        return len(sel.xpath(self.CAPTCHA_XPATH)) > 0
+        return len(sel.xpath(self.CAPTCHA_FORM_XPATH)) > 0 or len(sel.xpath(self.CAPTCHA_XPATH)) > 0
 
     @inlineCallbacks
     def handle_captcha(self, response, solver):
         sel = scrapy.Selector(response)
-        iframe_src = sel.xpath(self.CAPTCHA_XPATH).extract()[0]
-        iframe_url = urljoin(response.url, iframe_src)
-        iframe_request = scrapy.Request(iframe_url)
-        iframe_response = yield download(self.crawler, iframe_request)
-        iframe_sel = scrapy.Selector(iframe_response)
-        img_src, = iframe_sel.xpath('//img/@src').extract()[:1] or [None]
+        form = sel.xpath(self.CAPTCHA_FORM_XPATH)
+        if form:
+            container = form[0]
+            form_response = response
+        else:
+            iframe_src = sel.xpath(self.CAPTCHA_XPATH).extract()[0]
+            iframe_url = urljoin(response.url, iframe_src)
+            iframe_request = scrapy.Request(iframe_url)
+            iframe_response = yield download(self.crawler, iframe_request)
+            container = scrapy.Selector(iframe_response)
+            form_response = iframe_response
+        img_src, = container.xpath('//img/@src').extract()[:1] or [None]
         if img_src is None:
             raise DecaptchaError('No //img/@src found on CAPTCHA page')
-        img_url = urljoin(iframe_response.url, img_src)
+        img_url = urljoin(form_response.url, img_src)
         img_request = scrapy.Request(img_url)
         img_response = yield download(self.crawler, img_request)
         logger.info('CAPTCHA image downloaded, solving')
         captcha_text = yield solver.solve(img_response.body)
         logger.info('CAPTCHA solved: %s' % captcha_text)
         challenge_request = scrapy.FormRequest.from_response(
-            iframe_response, formxpath='//form',
+            form_response, formxpath='//form',
             formdata={'recaptcha_response_field': captcha_text}
         )
         challenge_response = yield download(self.crawler, challenge_request)
